24db33ff318522fdd47a6cd555215c7fad7c2e57
Natalia <natnlg8@gmail.com>
2025-03-10 20:29:06 -0700

    Natalia-classactivity2-10march #1

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ alert(hello World")
     function clickheading(){
         alert("heading is being clicked");
     }
+
+    
+function clickheading() {
+    alert("Heading is being clicked");
+}
+
+// alert("Hello world")
+
+function clickHeading(){
+    alert("Heading is beign clicked")
+}
+
+let name="Steven"
+let age=27
+let student=false
+let france=undefined
+let english=null
+
+console.log("Person name is " + name);
+console.log("Person age is " + age);
+console.log("the person is a student " + student);
+console.log("Person address is " + address);
+console.log("Person middle name is " + middleName);
+console.log("Person's country is " + country);
